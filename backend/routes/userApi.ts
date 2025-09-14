781e9ec2858e2617e9ecaba4d916e2fe95349941
Albert Hansen <alberthansen@Alberts-MacBook-Pro.local>
2025-09-14 15:06:06 +0200

    ESLint fix

--- backend/routes/userApi.ts
+++ backend/routes/userApi.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import User  from "../models/userModel.js";
+import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
 const router = Router();
