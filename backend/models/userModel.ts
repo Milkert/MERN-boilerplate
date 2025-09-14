c184489725a0fe3eee4494b8def1879c1ca6a4a7
Albert Hansen <alberthansen@Alberts-MacBook-Pro.local>
2025-09-14 15:04:26 +0200

    Bcrypt, login and signup backend and frontend - missing session. api also setup with react query

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
-const user = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = user;
+export default User;
